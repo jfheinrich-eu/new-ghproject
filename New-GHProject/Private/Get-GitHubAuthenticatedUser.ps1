e006b399e8390590d45a0058244688db4b7deaf8
J.F.Heinrich <contact@jfheinrich.eu>
2025-10-05 19:32:26 +0200

    Implement Get-GitHubAuthenticatedUser function

--- New-GHProject/Private/Get-GitHubAuthenticatedUser.ps1
+++ New-GHProject/Private/Get-GitHubAuthenticatedUser.ps1
@@ -1,19 +1,25 @@
 function Get-GitHubAuthenticatedUser {
     param(
+        # The GitHub personal access token, required for authentication
         [Parameter(Mandatory = $true)]
         [string]$Token
     )
 
+    # Set up the HTTP headers for the API request, including authorization and accepted response format
     $headers = @{
         'Authorization' = "token $Token"
         'Accept' = 'application/vnd.github.v3+json'
     }
 
     try {
+        # Send a GET request to GitHub's API to retrieve the authenticated user's information
         $user = Invoke-RestMethod -Uri "https://api.github.com/user" -Headers $headers
+
+        # Return the login name (username) of the authenticated user
         return $user.login
     }
     catch {
+        # If there is an error (e.g., invalid token), throw a descriptive error message
         throw "Failed to get authenticated user: $_"
     }
 }
